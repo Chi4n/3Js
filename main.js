ce76bd9dcc3fa9bffbd1f31006074bcb8efcd196
gianbaeza <gianb04@gmail.com>
2023-05-04 18:07:08 -0300

    Pagina de login

---
+++ main.js
@@ -0,0 +1,30 @@
+
+const logueo = document.getElementById ("botonLogin");
+
+const usuarioAutorizado = "Dear";
+const passwordAutorizado = "123";
+
+logueo.addEventListener ("click", ()=>{
+    Swal.fire ({
+        title: "Login",
+        html: ` <input type="text" id="usuario" class="swal2-input" placeholder="usuario">
+                <input type="text" id="password" class="swal2-input" placeholder="password">`,
+        confirmButtonText: "Enviar",
+        showCancelButton: true,
+        cancelButtonText: "Salir",
+    }) .then((result) =>{
+        if(result.isConfirmed){
+            const usuario= document.getElementById("usuario").value;
+            const password = document.getElementById("password").value;
+            Swal.fire({
+                title: "Sus datos no coinciden",
+                icon: "error",
+                confirmButtonText: "Aceptar",
+            })
+            if(usuario === usuarioAutorizado && password === passwordAutorizado){
+                 window.location.href = "mercado.html";
+             }
+        }
+    })
+})
+
